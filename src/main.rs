3f40eba492d39f67b07bce84173cd72eba25caef
Ivar Refsdal <ivar.refsdal@sikt.no>
2026-04-27 11:04:40 +0200

    handle logout

--- src/main.rs
+++ src/main.rs
@@ -667,6 +667,8 @@ fn run_daemon_vm(args: CliArgs, instance_dir: PathBuf) -> Result<(), Box<dyn std
         login_actions.push(motd_action);
     }
 
+    // if the vibe client attaching aborts _before_ actually logging in,
+    // we still want to shutdown the VM:
     const S: &str = " bash -c '(while true; do sleep 3; if [[ \"$(who | wc -l | tr -d \" \")\" == \"0\" ]]; then echo \"VM powering off...\"; systemctl poweroff; fi; done) 2>&1 &'";
     login_actions.push(Send(S.to_string()));
 
